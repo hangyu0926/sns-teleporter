83d00e7468cc93164f404d97c1cf60c87e4dfc34
kisho <kisho.dai@mi-me.com>
2017-04-29 16:30:02 +0800

    升级到2.0版本

---
+++ src/test/java/TeleporterMainTest.java
@@ -0,0 +1,29 @@
+/*
+ * -------------------------------------------------------------------------------------
+ * Mi-Me Confidential
+ *
+ * Copyright (C) 2015 Shanghai Mi-Me Financial Information Service Co., Ltd.
+ * All rights reserved.
+ *
+ * No part of this file may be reproduced or transmitted in any form or by any means,
+ * electronic, mechanical, photocopying, recording, or otherwise, without prior
+ * written permission of Shanghai Mi-Me Financial Information Service Co., Ltd.
+ * -------------------------------------------------------------------------------------
+ */
+
+import cn.memedai.orientdb.teleporter.TeleporterMain;
+
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Teleporter启动类
+ *
+ * @author kisho on 2017/4/26.
+ */
+public class TeleporterMainTest {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        TeleporterMain.main(args);
+    }
+
+}
